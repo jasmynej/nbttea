19aedeebc2cfcf1d4e441479a41d885bdd90cc06
jasmynej14 <jasmynej14@brandeis.edu>
2021-12-13 13:57:08 -0500

    init

--- nothing-but-the-tea/gatsby-config.js
+++ nothing-but-the-tea/gatsby-config.js
@@ -1,8 +1,20 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Nothing But The Tea",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    plugins: [
-
-    ]
-}
+  ],
+};
